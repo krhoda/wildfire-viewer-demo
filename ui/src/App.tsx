7dd5c38412d433550264ba9ac6df21d44a54c88e
Kelsey <kelsey@myvisionlabs.ai>
2025-07-31 12:48:38 -0600

    Cleaning frontend for use in hosting

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import type { Incoming } from './types/types'
-import { IncomingContianer } from './components/IncomingContainer'
 import WorldMap from './components/WorldMap'
 
 function App() {
@@ -31,22 +28,13 @@ function App() {
 
     ws.current.onmessage = e => {
       const message = JSON.parse(e.data);
-      console.log("MESSAGE:", message);
       setFires(message as Array<Incoming>);
     }
   }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>All Current US Wildfires</h1>
       <div className="card">
         {WorldMap(fires)}
         {/* {fires.length > 0 ? 
--- ui/src/components/IncomingContainer.tsx
+++
@@ -1,8 +0,0 @@
-import type { Incoming } from '../types/types'
-
-export function IncomingContianer(incoming: Incoming) {
-    return (<>
-        <p>Name: {incoming.properties.name}</p>
-        <p>X: {incoming.geometry.coordinates[0]}, Y: {incoming.geometry.coordinates[1]}</p>
-    </>)
-}
